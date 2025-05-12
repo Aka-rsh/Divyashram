163640ff00519d7407e1a97689ee440bfc0e85fa
Aka-rsh <98112532+Aka-rsh@users.noreply.github.com>
2025-05-12 08:36:20 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Divyashram
-Divyashram Trust
+"# DivyashramTrust" 
---
+++ set.js
@@ -0,0 +1,21 @@
+function setLanguage(lang) {
+  document.querySelectorAll('[data-en]').forEach(el => {
+    el.innerHTML = el.getAttribute(`data-${lang}`);
+  });
+}
+
+
+
+const topNav = document.querySelector('.top-nav');
+const mainHeader = document.querySelector('.main-header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    topNav.style.display = 'none';
+    mainHeader.style.top = '0';
+  } else {
+    topNav.style.display = 'block';
+    mainHeader.style.top = '30px'; // same as initial .top-nav height
+  }
+});
+
